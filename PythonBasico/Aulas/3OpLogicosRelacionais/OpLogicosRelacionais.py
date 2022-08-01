a44580ffa774eeb3a3041c82d999e53ca91b6827
Gabriel Nava <gabrielnavaemp@gmail.com>
2022-07-31 23:57:45 -0300

    Initial commit

---
+++ PythonBasico/Aulas/3OpLogicosRelacionais/OpLogicosRelacionais.py
@@ -0,0 +1,29 @@
+
+# Operadores Logicos
+a = True
+b = False
+
+print(a, b)
+print(type(a))
+
+
+print(a and b)  # and ou &
+
+
+c = a and b
+print("'A' e 'B' sao iguais eh", c)  # Tentar sempre utilizar aspas duplas
+
+print(a or b)  # or ou |
+
+d = a or b
+print("'A' ou 'B' eh igual a", d)
+
+print(not a)  # Not inverte o valor
+
+# Operadores Relacionais
+print(5 > 3)
+print(5 < 3)
+print(5 >= 3)
+print(5 <= 3)
+print(5 == 3)
+print(5 != 3)
