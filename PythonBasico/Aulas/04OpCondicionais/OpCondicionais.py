dfe9fb1db346017186b34e0a4c20dd791caa9275
gnavadev <gabrielnavaemp@gmail.com>
2022-08-03 17:32:06 -0300

    Updates

---
+++ PythonBasico/Aulas/04OpCondicionais/OpCondicionais.py
@@ -0,0 +1,28 @@
+print(5 > 3)
+if 5 > 3:
+    print('5 maior que 3')
+else:
+    print('MEU DEUS')
+
+
+if 5 > 6:
+    print('5 maior')
+else:
+    print('de novo')
+
+n = 4
+if n == 4:
+    print('n=4')
+else:
+    if n == 3:
+        print('N=3')
+    else:
+        print("n != de 4 e 3")
+
+x = 2
+y = 2
+
+if (x > 1) and (y % 2 == 0):
+    print('X eh maior que 1 e Y eh par')
+else:
+    print('Uma ou nenhuma das condicoes foram satisfeitas')
