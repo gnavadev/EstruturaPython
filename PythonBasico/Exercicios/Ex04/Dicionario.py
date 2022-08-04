cdd1ad4c913ad216f1746f88a777fd721dceca88
gnavadev <gabrielnavaemp@gmail.com>
2022-08-04 10:15:32 -0300

    folder fix

---
+++ PythonBasico/Exercicios/Ex04/Dicionario.py
@@ -0,0 +1,13 @@
+# Lista: Crie uma estrutura de repetição para fazer a leitura de 5 números inteiros e os armazene dentro de uma lista.
+# Após a leitura, crie outra estrutura de repetição para somar todos os valores digitados
+
+lista = []
+acumulador = 0
+for i in range(1, 6):
+    numeros = int(input("Digite um numero: "))
+    lista.append(numeros)
+print(lista)
+
+for item in lista:
+    acumulador = acumulador + item
+print(acumulador)
---
+++ PythonBasico/Exercicios/Ex04/Matriz.py
@@ -0,0 +1,15 @@
+# Matriz: Dada a matriz abaixo, construa uma estrutura de repetição para percorrer e somar todos os elementos da matriz
+# matriz = np.array([[3, 4, 1],
+#                    [3, 1, 5]])
+
+import numpy as np
+
+matriz = np.array([[3, 4, 1],
+                   [3, 1, 5]])
+acumulador = 0
+
+for i in range(matriz.shape[0]):
+    for j in range(matriz.shape[1]):
+        acumulador = acumulador + matriz[i][j]
+        print(matriz[i][j])
+print(acumulador)
