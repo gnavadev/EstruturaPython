daa2b44044551f6a8c8b0fc0cd93290131f3baba
gnavadev <gabrielnavaemp@gmail.com>
2022-08-04 10:23:21 -0300

    Merge Request

--- PythonBasico/Exercicios/Ex04/Lista.py
+++ PythonBasico/Exercicios/Ex04/Lista.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Crie um dicionário para armazenar o nome e a nota de 3 alunos, fazendo a leitura dos valores por meio de uma estrutura de repetição.
 # Depois, crie uma nova estrutura de repetição para somar todas as notas e retornar a média
 
@@ -14,20 +13,3 @@ for nome, nota in notas.items():
     media = media + nota
 media = media / 3
 print(media)
-=======
-# Crie um dicionário para armazenar o nome e a nota de 3 alunos, fazendo a leitura dos valores por meio de uma estrutura de repetição.
-# Depois, crie uma nova estrutura de repetição para somar todas as notas e retornar a média
-
-notas = {}
-media = 0
-for j in range(1, 4):
-    nome_aluno = str(input("Digite o Nome do Aluno: "))
-    nota_aluno = float(input("Digite a nota do aluno: "))
-    notas[nome_aluno] = nota_aluno
-print(notas.items())
-
-for nome, nota in notas.items():
-    media = media + nota
-media = media / 3
-print(media)
->>>>>>> e0de5cb675f7fef4e79db06ff7280c575441d54a
--- PythonBasico/Exercicios/Ex04/Matriz.py
+++ PythonBasico/Exercicios/Ex04/Matriz.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Matriz: Dada a matriz abaixo, construa uma estrutura de repetição para percorrer e somar todos os elementos da matriz
 # matriz = np.array([[3, 4, 1],
 #                    [3, 1, 5]])
@@ -14,20 +13,3 @@ for i in range(matriz.shape[0]):
         acumulador = acumulador + matriz[i][j]
         print(matriz[i][j])
 print(acumulador)
-=======
-# Matriz: Dada a matriz abaixo, construa uma estrutura de repetição para percorrer e somar todos os elementos da matriz
-# matriz = np.array([[3, 4, 1],
-#                    [3, 1, 5]])
-
-import numpy as np
-
-matriz = np.array([[3, 4, 1],
-                   [3, 1, 5]])
-acumulador = 0
-
-for i in range(matriz.shape[0]):
-    for j in range(matriz.shape[1]):
-        acumulador = acumulador + matriz[i][j]
-        print(matriz[i][j])
-print(acumulador)
->>>>>>> e0de5cb675f7fef4e79db06ff7280c575441d54a
