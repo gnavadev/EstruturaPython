6228e6c9b31d68462da05d14ba055a52932ee9a0
Gabriel Nava <gabrielnavaemp@gmail.com>
2022-08-04 10:16:59 -0300

    Delete PythonBasico/Exercicios/Ex1 directory

--- PythonBasico/Exercicios/Ex1/Ex1.py
+++
@@ -1,8 +0,0 @@
-a = int(input('Digite o primeiro numero: '))
-b = int(input('Digite o segundo numero: '))
-print(a, b)
-print("A soma eh: ", a + b)
-print("A subtracao eh: ", a - b)
-print("A divisao eh: ", a / b)
-print("A multiplicacao eh: ", a * b)
-print("O resto da divisao eh: ", a % b)
